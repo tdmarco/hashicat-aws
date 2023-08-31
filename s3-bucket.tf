d5130d039c6b9d24c683958fc7731e5a835d3996
Tony DeMarco <tdmarco@amazon.com>
2023-08-31 14:49:19 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/tdmarco/s3-bucket/aws"
+  version = "3.15.1"
   bucket-prefix = var.prefix
-
 }
